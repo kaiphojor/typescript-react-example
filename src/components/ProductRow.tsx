80f8a9f299e9bc6f98f5cdd66f608cfb163750ca
kaiphojor <jihoopark7666@gmail.com>
2023-04-25 19:17:29 +0900

    CategoryRow 분리

---
+++ src/components/ProductRow.tsx
@@ -0,0 +1,14 @@
+import type Product from '../types/Product';
+
+type ProductRowProps = {
+	product: Product;
+};
+
+export default function ProductRow({product}: ProductRowProps) {
+	return (
+		<tr>
+			<td>{product.name}</td>
+			<td>{product.price}</td>
+		</tr>
+	);
+}
--- src/components/ProductsInCategory.tsx
+++ src/components/ProductsInCategory.tsx
@@ -1,3 +1,6 @@
+import ProductRow from './ProductRow';
+import CategoryRow from './CategoryRow';
+
 import type Product from '../types/Product';
 
 type ProductsInCategoryProps = {
@@ -5,27 +8,16 @@ type ProductsInCategoryProps = {
 	products: Product[];
 };
 
-function ProductRow({product}: {product: Product}) {
-	return (
-		<tr>
-			<td>{product.name}</td>
-			<td>{product.price}</td>
-		</tr>
-	);
-}
-
-export default function ProductInCategory({category, products}: ProductsInCategoryProps) {
+export default function ProductInCategory({
+	category, products,
+}: ProductsInCategoryProps) {
 	const productsInCategory = products.filter(product => (
 		product.category === category
 	));
 
 	return (
 		<>
-			<tr>
-				<th colSpan={2}>
-					{category}
-				</th>
-			</tr>
+			<CategoryRow category={category} />
 			{productsInCategory.map(product => (
 				<ProductRow key={product.name} product={product} />
 			))}
