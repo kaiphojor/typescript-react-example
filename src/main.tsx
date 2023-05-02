118be2ac343ed42067c266f6b966b1626a05d66e
kaiphojor <jihoopark7666@gmail.com>
2023-05-02 16:43:15 +0900

    products fetch api로 expresss backend에서 가져오기

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-const element = document.getElementById('root');
+async function main() {
+	// Fetch
+	const url = 'http://localhost:3000/products';
+	const response = await fetch(url);
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const data = await response.json();
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const {products} = data;
+	console.log(products);
 
-if (element) {
+	const element = document.getElementById('root');
+
+	if (!element) {
+		return;
+	}
+
 	const root = ReactDOM.createRoot(element);
-
-	root.render(<App/>);
+	root.render((
+		<React.StrictMode>
+			<App/>
+		</React.StrictMode>
+	));
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+main();
