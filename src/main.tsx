99d7af451a033eab6b076a84e6fd06e7109ea01e
kaiphojor <jihoopark7666@gmail.com>
2023-05-25 21:30:06 +0900

    test : routes test

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
-import {BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom';
-import routes from './routes';
 
 function main() {
 	const element = document.getElementById('root');
@@ -11,14 +9,11 @@ function main() {
 		return;
 	}
 
-	const router = createBrowserRouter(routes);
-
 	const root = ReactDOM.createRoot(element);
 
 	root.render(
 		<React.StrictMode>
-			<RouterProvider router={router}/>
-			{/* <App/> */}
+			<App/>
 		</React.StrictMode>,
 	);
 }
--- src/routes.test.tsx
+++ src/routes.test.tsx
@@ -1,26 +1,25 @@
 import {render, screen} from '@testing-library/react';
 import {App} from './App';
-import {MemoryRouter} from 'react-router-dom';
+import {createMemoryRouter, MemoryRouter, RouterProvider} from 'react-router-dom';
+import routes from './routes';
 
 const context = describe;
 
 describe('App', () => {
 	context('when the current path is "/"', () => {
 		it('renders the home page', () => {
+			const router = createMemoryRouter(routes, {initialEntries: ['/']});
 			render((
-				<MemoryRouter initialEntries={['/']}>
-					<App/>
-				</MemoryRouter>
+				<RouterProvider router={router}/>
 			));
 			screen.getByText(/Welcome/);
 		});
 	});
 	context('when the current path is "/about"', () => {
 		it('renders the about page', () => {
+			const router = createMemoryRouter(routes, {initialEntries: ['/about']});
 			render((
-				<MemoryRouter initialEntries={['/about']}>
-					<App/>
-				</MemoryRouter>
+				<RouterProvider router={router}/>
 			));
 			screen.getByText(/This is test/);
 		});
