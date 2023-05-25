2db9f866ec5c9ee2d75518654cece79af70c1293
kaiphojor <jihoopark7666@gmail.com>
2023-05-25 22:01:54 +0900

    feat : session 기록 스택에 history.pushState()

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
+import {type SyntheticEvent} from 'react';
+
 export default function Header() {
+	const handleClick = (event: SyntheticEvent) => {
+		event.preventDefault();
+		const state = {};
+		const title = '';
+		const url = '/about';
+		window.history.pushState(state, title, url);
+	};
+
 	return (
 		<header>
 			<nav>
 				<ul>
 					<li>
-						<a href='/'>Home</a>
+						<a href='/' onClick={handleClick}>Home</a>
 					</li>
 					<li>
-						<a href='/about'>About</a>
+						<a href='/about' onClick={handleClick}>About</a>
 					</li>
 				</ul>
 			</nav>
