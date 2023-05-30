76f37647f6f97d1f10c6b77cec333b8665a9a271
kaiphojor <jihoopark7666@gmail.com>
2023-05-30 23:41:19 +0900

    feat : styled-rest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Greeting from './components/Greeting';
 import Switch from './components/Switch';
 
+import {Reset} from 'styled-reset';
+
 export default function App() {
 	return (
 		<div>
+			<Reset/>
 			<Greeting/>
 			<Switch/>
 		</div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -6,5 +7,9 @@ const element = document.getElementById('root');
 if (element) {
 	const root = ReactDOM.createRoot(element);
 
-	root.render(<App/>);
+	root.render(
+		<React.StrictMode>
+			<App/>
+		</React.StrictMode>,
+	);
 }
